991298094768db82b42df15379d1a777c392900f
rubert <guilherme@guilhermerubert.com>
2020-12-01 00:16:33 -0300

    Update directory

--- EasyCloud/Admin/includes/db_connect.php
+++
@@ -1,5 +0,0 @@
-﻿<?php
-error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
-include_once 'psl-config.php';   // Já que functions.php não está incluso
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
-?>
